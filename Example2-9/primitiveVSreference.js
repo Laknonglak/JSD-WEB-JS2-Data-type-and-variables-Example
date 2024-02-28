4e38ed36ba4550e4a2d611e74d9895710fe3b709
Laknonglak <donovanlak@gmail.com>
2024-02-28 12:59:38 +0000

    add primitive and reference example

---
+++ Example2-9/primitiveVSreference.js
@@ -0,0 +1,18 @@
+//Primivite
+let x = 10;
+let y = x;
+
+x = 20;
+
+console.log(x);
+console.log(y);
+
+
+// Reference
+let numOne = [50, 20, 70];
+let numTwo = numOne;
+
+numOne[0] = 100;
+
+console.log(numOne);
+console.log(numTwo);
